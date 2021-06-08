3bc5894ff8e58a95fa02cc8cee90b38a8314b744
AduohPhalange <houdaelbiache@gmail.com>
2021-06-08 19:46:18 +0100

    Modified DemoApplication.java

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.example.demo.DAOs.ArticleDAO;
-import com.example.demo.DAOs.GroupDAO;
-import com.example.demo.DAOs.ParticipantDAO;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = {GroupDAO.class,ParticipantDAO.class, ArticleDAO.class})
+@EnableJpaRepositories(basePackageClasses = {ArticleDAO.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
